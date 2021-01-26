bb6409c94e4d1e483e9804d37b0419de93041201
Alex Mole <alex.molen@gmail.com>
2021-01-26 04:49:40 +0000

    day32 whatamess

--- aoc20/day23.cpp
+++ aoc20/day23.cpp
@@ -1,79 +1,49 @@
 #include "pch.h"
 #include "harness.h"
 
-
-string day23(const string& input, int nmoves = 10)
+void kernel(vector<int>& state, const int nmoves)
 {
     const size_t npickedup = 3;
-    const size_t ncups = input.length();
-    
-    vector<uint8_t> state;
-    ranges::transform(input, back_inserter(state), [](const char c) -> uint8_t { return c - '0'; });
+    const size_t ncups = state.size();
 
     const int lo = *ranges::min_element(state);
     const int hi = *ranges::max_element(state);
 
-    string pickup;
-
-    for (int move = 1; move <= nmoves; ++move)
-    {
-        auto itpickup = begin(state) + 1;
-        pickup.assign(itpickup, itpickup + npickedup);
-        state.erase(itpickup, itpickup + npickedup);
-
-        int curr = state.front();
-        int dest = curr - 1;
-        for (;;)
-        {
-            if (dest < lo) dest = hi;
-            auto itfound = ranges::find(state, uint8_t(dest));
-            if (itfound == end(state))
-            {
-                --dest;
-                continue;
-            }
-
-            state.insert(itfound + 1, begin(pickup), end(pickup));
-            break;
-        }
-
-        ranges::rotate(state, begin(state) + 1);
-    }
-
-    auto itone = ranges::find(state, 1);
-    ranges::rotate(state, itone);
-
-    string result;
-    transform(begin(state) + 1, end(state), back_inserter(result), [](uint8_t i) -> char { return '0' + i; });
-
-    return result;
-}
-
-int64_t day23_2(const string& input, int nmoves = 10'000'000)
-{
-    const size_t npickedup = 3;
-    const size_t ncups = 1'000'000;
-
-    vector<int> state;
-    ranges::transform(input, back_inserter(state), [](const char c) -> int { return c - '0'; });
-    const int lo = *ranges::min_element(state);
-    int hi = *ranges::max_element(state);
-    state.resize(ncups, -1);
-    iota(begin(state) + input.size(), end(state), hi+1);
-    hi = state.back();
-
     size_t curr = 0;
     int pickup[3] = { -1, -1, -1 };
 
-   /* auto pick_up = [&state, &pickup, curr](size_t offs)
+    vector<int> indices(ncups + 1, -1);
+    auto reindex_full = [&state, &indices]()
     {
-        size_t ix = (curr + 1 + offs);
-        if (ix >= ncups)
-            ix -= ncups;
+        int ix = 0;
+        for (auto cup : state)
+        {
+            indices[cup] = ix;
+            ++ix;
+        }
+    };
+    auto reindex_partial = [&state, &indices](vector<int>::iterator itstart, vector<int>::iterator itend)
+    {
+        int ix = int(distance(begin(state), itstart));
+        while (itstart != itend)
+        {
+            indices[*itstart] = ix;
+            ++ix;
+            ++itstart;
+        }
+    };
 
-        pickup[offs] = state[ix];
-        state[ix] = -1;
-    };*/
+    reindex_full();
+
+    /* auto pick_up = [&state, &pickup, curr](size_t offs)
+     {
+         size_t ix = (curr + 1 + offs);
+         if (ix >= ncups)
+             ix -= ncups;
+
+         pickup[offs] = state[ix];
+         state[ix] = -1;
+     };*/
 
     for (int move = 1; move <= nmoves; ++move)
     {
@@ -89,6 +59,8 @@ int64_t day23_2(const string& input, int nmoves = 10'000'000)
         for (;;)
         {
             if (dest < lo) dest = hi;
+            //auto itdest = begin(state) + indices[dest];
+            //if (*itdest == -1)
             auto itdest = ranges::find(state, dest);
             if (itdest == end(state))
             {
@@ -100,8 +72,29 @@ int64_t day23_2(const string& input, int nmoves = 10'000'000)
             {
                 auto itstart = itpickup + npickedup;
                 auto itend = itdest + 1;
-                ::move(itstart, itend, itpickup);
-                ranges::copy(pickup, itend - npickedup);
+
+                //::move(itstart, itend, itpickup);
+                auto itin = itstart;
+                auto itout = itpickup;
+                int ix = int(distance(begin(state), itout));
+                for (; itin != itend; ++itin, ++itout, ++ix)
+                {
+                    int x = *itin;
+                    *itout = x;
+                    //indices[x] = ix;
+                }
+
+                //ranges::copy(pickup, itend - npickedup);
+                auto itin2 = begin(pickup);
+                auto itend2 = end(pickup);
+                for (; itin2 != itend2; ++itin2, ++itout, ++ix)
+                {
+                    int x = *itin2;
+                    *itout = x;
+                   // indices[x] = ix;
+                }
+                
+                //reindex_partial(itpickup, itend);
             }
             else
             {
@@ -109,6 +102,9 @@ int64_t day23_2(const string& input, int nmoves = 10'000'000)
                 auto itend = itpickup;
                 ::move(itstart, itend, itstart + npickedup);
                 ranges::copy(pickup, itstart);
+              // reindex_partial(itstart, itend + npickedup);
+
+                curr += npickedup;
             }
             break;
         }
@@ -116,8 +112,42 @@ int64_t day23_2(const string& input, int nmoves = 10'000'000)
         ++curr;
         if (curr == ncups) curr = 0;
         else if (curr + npickedup >= ncups)
+        {
             ranges::rotate(state, begin(state) + curr);
+            curr = 0;
+            //reindex_full();
+        }
     }
+}
+
+string day23(const string& input, int nmoves = 10)
+{
+    vector<int> state;
+    ranges::transform(input, back_inserter(state), [](const char c) -> int { return c - '0'; });
+
+    kernel(state, nmoves);
+
+    auto itone = ranges::find(state, 1);
+    ranges::rotate(state, itone);
+
+    string result;
+    transform(begin(state) + 1, end(state), back_inserter(result), [](int i) -> char { return '0' + char(i); });
+
+    return result;
+}
+
+int64_t day23_2(const string& input, int nmoves = 10'000'000)
+{
+    const int ncups = 1'000'000;
+
+    vector<int> state;
+    ranges::transform(input, back_inserter(state), [](const char c) -> int { return c - '0'; });
+    int hi = *ranges::max_element(state);
+    state.resize(ncups, -1);
+    iota(begin(state) + input.size(), end(state), hi+1);
+    hi = state.back();
+
+    kernel(state, nmoves);
 
     auto itone = ranges::find(state, 1);
     int64_t result = *(++itone);
@@ -133,7 +163,7 @@ void run_day23()
 
     test("92658374", day23(sample));
     test("67384529", day23(sample, 100));
-    gogogo(day23(input, 100));
+    gogogo(day23(input, 100), "25368479"s);
 
     test(149245887792ll, day23_2(sample));
     //gogogo(day23_2(LOAD(23)));
